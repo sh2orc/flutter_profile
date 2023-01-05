1c5930e0ccac17259696e07e464a594e6d9fdc48
Lee Taeyoung <seen@korea.ac.kr>
2023-01-05 22:12:42 +0900

    initial commit

---
+++ lib/components/profile_header.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width: 20,),
+        SizedBox(width: 20,),
+      ],
+    );
+  }
+
+  Widget _buildHeaderAvatar(){
+    return SizedBox();
+  }
+
+  Widget _buildHeaderProfile(){
+    return SizedBox();
+  }
+}
